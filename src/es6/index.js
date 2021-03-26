167ac9a813899dc0c142c0a5378f6f6e0499ec7f
Alberto Fuentes <alberto.fuentesco@gmail.com>
2021-03-25 19:26:25 -0600

    Clase de LET y CONST, Multilínea, Spread Operator y Desestructuración

--- src/es6/index.js
+++ src/es6/index.js
@@ -26,3 +26,29 @@ console.log(epicPhrase);
 //es6
 let epicPhrase2 = `${hello} ${world}`
 console.log(epicPhrase2)
+
+//multilínea antes de ec6
+let lorem = "Qui consequatur . Commodi. Ipsum vel duis yet minima  \n"
++ "otra frase que necesitamos"
+
+//ec6
+let lorem2 = `otra frase epica que necesitamos
+ahora es otra frase épica`;
+
+console.log(lorem)
+console.log(lorem2)
+
+let person = {
+    "name": "Beto",
+    "age": 32,
+    "Country": "MX"
+}
+
+//Antes de ecs6
+console.log(person.name, person.age);
+//ecs6
+let { name, age, country } = person;
+console.log(name, age);
+
+let team1 = ['Oscar', 'Julian', 'Ricardo'];
+let team2 = ['Valeria', 'Yesica', 'Camila'];
