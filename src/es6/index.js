fcdbbe2f2609e8df3e6ab1c078d82a0009898fa7
Alberto Fuentes <alberto.fuentesco@gmail.com>
2021-03-25 19:41:53 -0600

    Clase de ArrowFunctions, Promesas y Parámetros en objetos

--- src/es6/index.js
+++ src/es6/index.js
@@ -50,5 +50,72 @@ console.log(person.name, person.age);
 let { name, age, country } = person;
 console.log(name, age);
 
+
+//Spread Operator (...)
 let team1 = ['Oscar', 'Julian', 'Ricardo'];
 let team2 = ['Valeria', 'Yesica', 'Camila'];
+
+
+let education = ['David',...team1, ...team2 ];
+
+console.log(education);
+
+//let: variable que solo funciona en su scope
+{
+    var globalvar = "Global Var";
+}
+
+{
+    let globallet = "Global Let";
+    console.log(globallet);
+}
+
+console.log(globalvar);
+
+//const: Variable constante, no se puede volver a asignar.
+const a = 'b';
+a = 'a';
+
+
+let name = 'Beto';
+let age = 23;
+//ecs5
+obj = {name: name, age: age};
+//ecs6
+obj2 = {name, age};
+console.log(obj2);
+
+
+//ARROW FUNCTIONS
+const names = [
+    {name : 'Beto', age: 23},
+    {name: 'Yessica', age: 27}
+]
+
+let listOfNames = names.map(function (item){
+    console.log(item.name);
+})
+
+//La asignación "=>" establece que es una función anónima.
+let listOfNames2 = names.map(item => console.log(item.name))
+
+const listOfNames3 = (name, age, country) => {
+    //bloque de código
+}
+
+const listOfNames4 = name => {
+    //bloque de código
+}
+
+const square = num => num * num;
+
+//PROMESAS
+const helloPromise = () => {
+    return new Promise((resolve, reject) => {
+        if(true){
+            resolve('Hey! promesa resuelta.')
+        } else {
+            reject('Oops!!');
+        }
+    });
+}
